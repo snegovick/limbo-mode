3261b43b1db34daa2a8ef95fc5cf3dd474339063
Kirik snegovick Konstantin <snegovick@gmail.com>
2015-03-01 09:41:05 +0300

    fixing indentation and paren matching

--- limbo-mode.el
+++ limbo-mode.el
@@ -22,43 +22,105 @@
   (beginning-of-line)
   (if (bobp)
       (indent-line-to 0)
-    (let ((not-indented t) (looking-at-open-paren nil) cur-indent)
-      (if (looking-at "^[ \t]*}")
-          (progn
-            (save-excursion
-              (forward-line -1)
-              (setq cur-indent (- (current-indentation) default-tab-width)))
-            (if (< cur-indent 0)
-                (setq cur-indent 0)))
-        (save-excursion 
-          (while not-indented
-            (if (looking-at "^[ \t]*{")
-                (setq looking-at-open-paren t))
-            (forward-line -1)
-            (if (looking-at "[ \t]*\\(=>\\|case\\|else\\|for\\|if\\|while\\)[ \t]*{")
+    (let ((not-indented t)
+          (looking-at-open-paren nil)
+          (looking-at-keyword nil)
+          (looking-at-keyword-open-paren nil)
+          (looking-at-close-paren nil)
+          (looking-at-start nil)
+          (looking-at-regular-line nil)
+          (distance 0)
+          cur-indent)
+      (save-excursion 
+        (if (looking-at "^[ \t]*{")
+            (progn
+              (message "looking at open paren")
+              (setq looking-at-open-paren t))
+          (if (looking-at "[ \t]*\\(=>\\|case\\|else\\|for\\|if\\|while\\).*[ \t]*{")
+              (progn
+                (message "looking at keyword open paren")
+                (setq looking-at-keyword-open-paren t))
+            (if (looking-at "[ \t]*\\(=>\\|case\\|else\\|for\\|if\\|while\\)")
                 (progn
-                  (setq cur-indent (+ (current-indentation) default-tab-width))
-                  (setq not-indented nil))
-              (if (looking-at "[ \t]*\\(=>\\|case\\|else\\|for\\|if\\|while\\)")
+                  (message "looking at keyword")
+                  (setq looking-at-keyword t))
+              (if (looking-at "^[ \t]*}")
                   (progn
-                    (if (equal looking-at-open-paren t)
-                        (progn
-                          (setq cur-indent (current-indentation))
-                          (setq not-indented nil))
-                      (progn
-                        (setq cur-indent (+ (current-indentation) default-tab-width))
-                        (setq not-indented nil))
-                      ))
-                (if (looking-at "^[ \t]*}")
+                    (message "looking at close paren")
+                    (setq cur-indent (- (current-indentation) default-tab-width))
+                    (setq looking-at-close-paren t))
+                (if (bobp)
                     (progn
+                      (message "looking at start")
+                      (setq looking-at-start t))
+                  (progn
+                    (message "looking at regular line")
+                    (setq looking-at-regular-line t)))
+                )
+              )
+            )
+          )
+        (while not-indented
+          (forward-line -1)
+          (setq distance (+ distance 1))
+          (if (looking-at "[ \t]*\\(case\\|else\\|for\\|if\\|while\\).*[ \t]*{")
+              (progn
+                (message "in open paren keyword")
+                (if (equal looking-at-close-paren t)
+                    (progn 
                       (setq cur-indent (current-indentation))
                       (setq not-indented nil))
-                  (if (looking-at "^[ \t]*{")
+                  (progn 
+                    (setq cur-indent (+ (current-indentation) default-tab-width))
+                    (setq not-indented nil))))
+            (if (looking-at "[ \t]*\\(case\\|else\\|for\\|if\\|while\\)")
+                (if (equal looking-at-open-paren t)
+                    (progn
+                      (message "open paren in keyword")
+                      (setq cur-indent (current-indentation))
+                      (setq not-indented nil))
+                  (if (equal looking-at-close-paren t)
                       (progn
-                        (setq cur-indent (+ (current-indentation) default-tab-width))
-                        (setq not-indented nil))
+                        (message "close paren in keyword")
+                                        ;(setq cur-indent (current-indentation))
+                                        ;(setq cur-indent (- (current-indentation) default-tab-width))
+                                        ;(setq not-indented nil)
+                        )
+                    (if (= distance 1)
+                        (progn
+                          (message "distance in keyword")
+                          (setq cur-indent (+ (current-indentation) default-tab-width))
+                          (setq not-indented nil))
+                      (progn
+                        (message "else in keyword")
+                        (setq cur-indent (current-indentation))
+                        (setq not-indented nil)))))
+              (if (looking-at ".*{.*}")
+                  ()
+                (if (looking-at ".*{")
+                    (progn
+                      (message "in open paren")
+                      (if (equal looking-at-close-paren t)
+                          (progn 
+                            (setq cur-indent (current-indentation))
+                            (setq not-indented nil))
+                        (progn 
+                          (setq cur-indent (+ (current-indentation) default-tab-width))
+                          (setq not-indented nil))))
+                  (if (looking-at "^[ \t]*}")
+                      (progn
+                        (message "in close paren")
+                        (if (equal looking-at-close-paren t)
+                            (progn
+                              (setq cur-indent (- (current-indentation) default-tab-width))
+                              (setq not-indented nil))
+                          (progn 
+                              (setq cur-indent (current-indentation))
+                              (setq not-indented nil))))
                     (if (bobp)
-                        (setq not-indented nil)
+                        (progn
+                          (message "in start")                          
+                          (setq not-indented nil))
                       )
                     )
                   )
@@ -69,12 +131,20 @@
         )
       (if cur-indent
           (indent-line-to cur-indent)
-        (indent-line-to 0))))) ; If we didn't see an indentation hint, then allow no indentation
+        (progn
+          (message "no hint")
+          (indent-line-to 0)))))) ; If we didn't see an indentation hint, then allow no indentation
 
 (defvar limbo-mode-syntax-table
   (let ((st (make-syntax-table)))
     (modify-syntax-entry ?# "< b" st)
     (modify-syntax-entry ?\n "> b" st)
+    (modify-syntax-entry ?\[ "(]" st)
+    (modify-syntax-entry ?\] ")[" st)
+    (modify-syntax-entry ?\{ "(}" st)
+    (modify-syntax-entry ?\} "){" st)
+    (modify-syntax-entry ?\( "()" st)
+    (modify-syntax-entry ?\) ")(" st)
     st)
   "Syntax table for limbo-mode")
 
